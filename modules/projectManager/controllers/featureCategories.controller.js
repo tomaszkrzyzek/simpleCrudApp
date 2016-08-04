e6fdb2c03652ba8d4753d0db41c3ff286f3f4238
Tomasz Krzyzek <tomasz.krzyzek@gmail.com>
2016-08-04 13:00:09 +0200

    Add FeatureCategories parsing to clean JSON before sending

--- modules/projectManager/controllers/featureCategories.controller.js
+++ modules/projectManager/controllers/featureCategories.controller.js
@@ -2,42 +2,49 @@ var express = require('express');
 var service = require('./../services/featureCategories.service');
 
 function deleteFeatureCategory(req, res, next) {
- var id = req.params[0];
- service.deleteFeatureCategory(id).then(function(data){
-  res.send(data[0]);
-  });
+    var id = req.params[0];
+    service.deleteFeatureCategory(id).then(function(data) {
+        res.send(data[0]);
+    });
 }
 
-function getFeatureCategory (req, res, next) {
- var id = req.params[0];
- service.getFeatureCategory(id).then(function(data){
-  res.send(data[0]);
- });
+function getFeatureCategory(req, res, next) {
+    var id = req.params[0];
+    service.getFeatureCategory(id).then(function(data) {
+        res.send(data[0]);
+    });
 }
 
 function getAllFeatureCategory(req, res, next) {
- service.getAllFeatureCategory().then(function(data){
-  res.send(data);
- });
+    service.getAllFeatureCategory().then(function(data) {
+        var fs = [];
+        data.forEach(function (r){
+          fs.push({
+            id: r.n._id,
+            name: r.n.properties.category
+          });
+        });
+        res.send(fs);
+    });
 }
 
 function createFeatureCategory(req, res, next) {
- service.createFeatureCategory(req.body.category).then(function(data){
-  res.send(data[0]);
- });
+    service.createFeatureCategory(req.body.category).then(function(data) {
+        res.send(data[0]);
+    });
 }
 
 function updateFeatureCategory(req, res, next) {
-  var id = req.params[0];
-  service.updateFeatureCategory(id, req.body.category).then(function(data){
-  res.send(data[0]);
- });
+    var id = req.params[0];
+    service.updateFeatureCategory(id, req.body.category).then(function(data) {
+        res.send(data[0]);
+    });
 }
 
 module.exports = {
-  getFeatureCategory: getFeatureCategory,
-  updateFeatureCategory: updateFeatureCategory,
-  createFeatureCategory: createFeatureCategory,
-  deleteFeatureCategory: deleteFeatureCategory,
-  getAllFeatureCategory: getAllFeatureCategory
+    getFeatureCategory: getFeatureCategory,
+    updateFeatureCategory: updateFeatureCategory,
+    createFeatureCategory: createFeatureCategory,
+    deleteFeatureCategory: deleteFeatureCategory,
+    getAllFeatureCategory: getAllFeatureCategory
 };
--- modules/projectManager/controllers/projects.controller.js
+++ modules/projectManager/controllers/projects.controller.js
@@ -2,55 +2,67 @@ var express = require('express');
 var service = require('./../services/projects.service');
 
 function deleteProject(req, res, next) {
- var id = req.params.id;
- service.deleteProject(id).then(function(data){
-  res.send(data[0]);
-  });
+    var id = req.params.id;
+    service.deleteProject(id).then(function(data) {
+        res.send(data[0]);
+    });
 }
 
-function getProject (req, res, next) {
- var id = req.params.id;
- service.getProject(id).then(function(data){
-  res.send(data[0]);
- });
+function getProject(req, res, next) {
+    var id = req.params.id;
+    service.getProject(id).then(function(data) {
+        res.send(data[0]);
+    });
 }
 
 function getProjectNumber(req, res, next) {
- service.getProjectNumber().then(function(data){
-  res.send(data[0]);
- });
+    service.getProjectNumber().then(function(data) {
+        res.send(data[0]);
+    });
 }
 
 function createProject(req, res, next) {
- service.createProject(req.body.name, req.body.identifier, req.body.description).then(function(data){
-  res.send(data[0]);
- });
+    service.createProject(req.body.name, req.body.identifier, req.body.description).then(function(data) {
+        res.send(data[0]);
+    });
 }
 
 function updateProject(req, res, next) {
-  var id = req.params.id;
-  service.updateProject(id, req.body.name, req.body.identifier, req.body.description).then(function(data){
-  res.send(data[0]);
- });
+    var id = req.params.id;
+    service.updateProject(id, req.body.name, req.body.identifier, req.body.description).then(function(data) {
+        res.send(data[0]);
+    });
 }
 
 function getProjectPage(req, res, next) {
-  var fromItem = req.query.from;
-  var toItem = req.query.to;
-  var projectQuery = req.query.query;
-  if(!projectQuery){
-    projectQuery = '';
-  }
-  service.getProjectPage(fromItem, toItem, projectQuery).then(function(data){
-  res.send(data);
- });
+    var fromItem = req.query.from;
+    var toItem = req.query.to;
+    var projectQuery = req.query.query;
+    if (!projectQuery) {
+        projectQuery = '';
+    }
+    service.getProjectPage(fromItem, toItem, projectQuery).then(function(data) {
+        var projects = [];
+        data.forEach(function(r) {
+            projects.push({
+                id: r.n._id,
+                name: r.n.properties.name,
+                identifier: r.n.properties.identifier,
+                description: r.n.properties.description
+            });
+
+        });
+        res.send(
+            projects
+        );
+    });
 }
 
 module.exports = {
-  getProjectPage: getProjectPage,
-  getProject: getProject,
-  updateProject: updateProject,
-  createProject: createProject,
-  deleteProject: deleteProject,
-  getProjectNumber: getProjectNumber
+    getProjectPage: getProjectPage,
+    getProject: getProject,
+    updateProject: updateProject,
+    createProject: createProject,
+    deleteProject: deleteProject,
+    getProjectNumber: getProjectNumber
 };
